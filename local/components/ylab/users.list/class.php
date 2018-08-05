5c2330c7d3ffae9f0c16bda54c93f09201744a37
vinter <wbmarsian@gmail.com>
2018-08-05 17:52:50 +0300

    change method getUsersList

--- local/components/ylab/users.list/class.php
+++ local/components/ylab/users.list/class.php
@@ -1,9 +1,25 @@
 <?php
 
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\Loader;
+use Bitrix\Main\LoaderException;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+
+/**
+ * Class UsersListComponent
+ */
 class UsersListComponent extends \CBitrixComponent
 {
+    /**
+     * @return mixed|void
+     */
     public function executeComponent()
     {
+        /**
+         * @global \CMain $APPLICATION
+         */
         global $APPLICATION;
 
         $APPLICATION->RestartBuffer();
@@ -12,21 +28,34 @@ class UsersListComponent extends \CBitrixComponent
         $this->includeComponentTemplate();
     }
 
+    /**
+     * @return array
+     */
     protected function getUsersList()
     {
-        return [
-            [
-                'ID' => 1,
-                'NAME' => 'Иван'
-            ],
-            [
-                'ID' => 2,
-                'NAME' => 'Петр'
-            ],
-            [
-                'ID' => 3,
-                'NAME' => 'Евгений'
-            ]
+        try {
+            Loader::includeModule("iblock");
+        } catch (LoaderException $e) {
+            echo $e->getMessage();
+        }
+
+        $arResult = [];
+
+        $params = [
+            'select' => ['ID', 'NAME'],
+            'filter' => ['IBLOCK_ID' => 1]
         ];
+
+        try {
+            $arResult = ElementTable::getList($params)->fetchAll();
+        } catch (ObjectPropertyException $e) {
+            echo $e->getMessage();
+        } catch (ArgumentException $e) {
+            echo $e->getMessage();
+        } catch (SystemException $e) {
+            echo $e->getMessage();
+        }
+
+        return $arResult;
     }
 }
